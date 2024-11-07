ad9acd9bad5eeea44ec513fffa5393e14efad77c
nikita <nikitakerzhik@gmail.com>
2024-11-07 17:52:05 +1000

    edit event_item

--- app/src/main/java/com/example/schedulefefu/Schedule.kt
+++ app/src/main/java/com/example/schedulefefu/Schedule.kt
@@ -2,22 +2,29 @@ package com.example.schedulefefu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.schedulefefu.adapter.EventAdapter
 import com.example.schedulefefu.event.EventGenerator
+import kotlinx.coroutines.launch
 
 class Schedule : AppCompatActivity() {
+
+    private lateinit var eventsAdapter: EventAdapter
+    private lateinit var eventsRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val eventsRecycler: RecyclerView = findViewById(R.id.eventsRecycler)
-        val eventsAdapter = EventAdapter()
-
-        eventsRecycler.layoutManager = LinearLayoutManager(this)
-        eventsRecycler.adapter = eventsAdapter
-
+        eventsRecyclerView = findViewById(R.id.eventsRecycler)
+        eventsAdapter = EventAdapter()
+        initOperationsRecycler()
         eventsAdapter.data = EventGenerator.generateEvents(10)
+    }
+
+    private fun initOperationsRecycler() {
+        eventsRecyclerView.layoutManager = LinearLayoutManager(baseContext)
+        eventsRecyclerView.adapter = eventsAdapter
     }
 }
